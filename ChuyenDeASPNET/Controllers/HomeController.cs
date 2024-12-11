e68b2b7d9a25aeb8ec207c4b4ba5f35e5671d7a3
VoThanhDat21072004 <vothanhdat12a9.hvt@gmail.com>
2024-12-11 17:57:39 +0700

    Resolve conflict

--- ChuyenDeASPNET/Controllers/HomeController.cs
+++ ChuyenDeASPNET/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChuyenDeASPNET.Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,11 @@ namespace ChuyenDeASPNET.Controllers
 {
     public class HomeController : Controller
     {
+        ASPNETEntities objASPNETEntities = new ASPNETEntities();
         public ActionResult Index()
         {
-            return View();
+            var lstProduct = objASPNETEntities.Products.ToList();
+            return View(lstProduct);
         }
     }
 }
