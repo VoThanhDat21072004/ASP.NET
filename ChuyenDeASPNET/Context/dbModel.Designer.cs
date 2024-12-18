6c576505f35f9b6833256e20bdc3cb0be4ee1d0c
VoThanhDat21072004 <vothanhdat12a9.hvt@gmail.com>
2024-12-18 16:15:00 +0700

    Resolve conflict

--- ChuyenDeASPNET/Context/dbModel.Designer.cs
+++ ChuyenDeASPNET/Context/dbModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\LapTrinhASP.NET\ChuyenDeASPNET\ChuyenDeASPNET\Context\dbModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\ASUS\Desktop\ASP.NET\ChuyenDeASPNET\Context\dbModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
---
+++ ChuyenDeASPNET/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ChuyenDeASPNET.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class MyDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+
+}
